47927e84250d2aeb1d47b9426a482765c6d8a693
Jody <smile.jody@gmail.com>
2019-05-06 09:25:51 -0400

    Store user quips for duration of game

--- server/socket-utils/socket-utils.js
+++ server/socket-utils/socket-utils.js
@@ -80,11 +80,11 @@ function getPairs(players, round) {
 
 /* TODO: Test cases
 getPrompts(4, 5);
-console.log(getPairs(['p1', 'p2', 'p3'], 3)); */
+console.log(getPairs(['p1', 'p2', 'p3'], 3)); 
 
 console.log(getPairs(['p1', 'p2', 'p3'], 1)); 
 console.log(getPairs(['p1', 'p2', 'p3'], 2)); 
-console.log(getPairs(['p1', 'p2', 'p3'], 3)); 
+console.log(getPairs(['p1', 'p2', 'p3'], 3)); */
 
 //getRandomPrompts(5);
 //console.log(getRandomPrompts(5));
