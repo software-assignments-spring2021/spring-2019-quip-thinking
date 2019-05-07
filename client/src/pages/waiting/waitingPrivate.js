728a95d16d98ec9ab780a754baf542406d67ad3a
Disha Gupta <dishagupta275@gmail.com>
2019-05-06 23:36:18 -0400

    changed file name

--- client/src/pages/waiting/waitingPrivate.js
+++ client/src/pages/waiting/waitingPrivate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './waiting.css';
-import "../games/TImer.js";
+import "../games/Timer.js";
 import {Button} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom'
 import socket, { getPlayers, subscribeToJoins } from '../../utils/api'
