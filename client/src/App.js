6857a0a2880bb9b1c6c5925e239e107fcf84d154
rebecca112233 <rebecca.shi@nyu.edu>
2019-05-06 22:06:08 -0400

    updated routing

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import Header from "./components/header";
 import JoinRoom from "./pages/join/JoinRoom";
 import CreatePrivate from "./pages/create/CreatePrivate";
 import Error from "./pages/error/Error";
-import Game from "./pages/games/Game";
+import Vote from "./pages/games/Vote";
+import Exit from "./pages/games/Exit";
+import Scoreboard from "./pages/games/Scoreboard";
 import WaitingPrivate from './pages/waiting/waitingPrivate';
 
 const App = () => (
@@ -18,21 +20,18 @@ const App = () => (
           <Route exact path="/" component={Home}/>
           <Route path = "/create/private" component = {CreatePrivate}/>
           <Route path = "/waiting/private" render={({
-            location : {
-              state : {
-                roomName,
-                playerName,
-                roomCode
-              }
-            }
+            location : {state : {roomName, playerName, roomCode}}
           }) => <WaitingPrivate
                   roomCode={roomCode}
                   roomName={roomName}
                   playerName={playerName}
                 />}
           />
+          {/* <Route path = "/answer/private" component = {}/> */}
+          <Route path = "/vote/private" component = {Vote}/>
+          <Route path = "/scoreboard/private" component = {Scoreboard}/>
+          <Route path = "/exit/private" component = {Exit}/>
           <Route path = "/join/public" component={JoinRoom}/>
-          <Route path = "/Game" component={Game} />
           <Route component={Error}/>
         </Switch>
       </Router>
--- client/src/pages/games/Exit.js
+++ client/src/pages/games/Exit.js
@@ -1,32 +1,28 @@
 import React from "react";
-import "./create.css";
+import "./game.css";
 import { Form, Row, Col } from "react-bootstrap";
-import Header from "../../components/header";
 
-const ExitPages = () => {
-	return (
-
-		<><Header>
-
-		<div className="exit">
-
-			<Form>
-				<h1>Continue Playing?</h1> <br/>
-
-				<Row>
-					<Col>
-						<span class="close">&times;</span>
-						<p> play with the same people </p>
-					</Col>
-					<Col>
-						<span class="close">&times;</span>
-						<p> exit </p>
-					</Col>
-				</Row>
-			</Form>
-
-		</div>
-	)
+class Exit extends React.Component{
+  render(){
+    return (
+      <div className="exit">
+        <Form>
+          <h1>Continue Playing?</h1> <br/>
+          <Row>
+            <Col>
+              <span class="close">&times;</span>
+              <p> Play with the same people </p>
+            </Col>
+            <Col>
+              <span class="close">&times;</span>
+              <p> Exit </p>
+            </Col>
+          </Row>
+        </Form>
+  
+      </div>
+    )
+  }
 }
 
-export default ExitPages;
+export default Exit;
