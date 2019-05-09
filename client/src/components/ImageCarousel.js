377ac1241ee75b4675052fc62a157da217531148
Disha Gupta <dishagupta275@gmail.com>
2019-05-08 23:34:35 -0400

    updated carousel

--- client/src/components/ImageCarousel.js
+++ client/src/components/ImageCarousel.js
@@ -24,13 +24,16 @@ export default class ImageCarousel extends React.Component {
       return (
         <Carousel activeIndex={index} direction={direction} onSelect={this.handleSelect} style={{width:"400px", height:"300px"}}>
           <Carousel.Item>
-            <img className="d-block w-100" src={require('./slide1.jpg')} alt="First slide"/>
+            <img className="d-block w-100" src={require('./QT1.jpg')} alt="first slide"/>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={require('./slide2.jpg')} alt="Second slide"/>
+            <img className="d-block w-100" src={require('./QT2.jpg')} alt="second slide"/>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={require('./slide3.jpg')} alt="Third slide"/>
+            <img className="d-block w-100" src={require('./QT3.jpg')} alt="third slide"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={require('./QT4.jpg')} alt="fourth slide"/>
           </Carousel.Item>
         </Carousel>
       );
